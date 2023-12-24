7d9690b198ba3a37f925d2a9424380d2f0796291
Elias Mina <hukitan@hotmail.com>
2023-12-24 10:50:48 -0600

    Modificacion para esperarel envio de mensajes, ya que el tiempo anterior era muy rapido y se crasheaba WA

--- shrekSendScript.js
+++ shrekSendScript.js
@@ -1,24 +1,24 @@
-async function enviarScript(scriptText){
+async function enviarScript(scriptText) {
 	const lines = scriptText.split(/[\n\t]+/).map(line => line.trim()).filter(line => line);
 	main = document.querySelector("#main"),
-	textarea = main.querySelector(`div[contenteditable="true"]`)
-	
-	if(!textarea) throw new Error("Não há uma conversa aberta")
-	
-	for(const line of lines){
+		textarea = main.querySelector(`div[contenteditable="true"]`)
+
+	if (!textarea) throw new Error("Não há uma conversa aberta")
+
+	for (const line of lines) {
 		console.log(line)
-	
+
 		textarea.focus();
 		document.execCommand('insertText', false, line);
-		textarea.dispatchEvent(new Event('change', {bubbles: true}));
-	
+		textarea.dispatchEvent(new Event('change', { bubbles: true }));
+
 		setTimeout(() => {
 			(main.querySelector(`[data-testid="send"]`) || main.querySelector(`[data-icon="send"]`)).click();
-		}, 100);
-		
-		if(lines.indexOf(line) !== lines.length - 1) await new Promise(resolve => setTimeout(resolve, 250));
+		}, 1000);
+
+		if (lines.indexOf(line) !== lines.length - 1) await new Promise(resolve => setTimeout(resolve, 1500));
 	}
-	
+
 	return lines.length;
 }
 
